80670cd8b7c7ca5cb74438854e3eba73eaffddfa
Olocool17 <ovanwassenhove@gmail.com>
2022-03-02 18:09:06 +0100

    Remove remnants, further configure PWM

--- src/main.c
+++ src/main.c
@@ -4,35 +4,17 @@
 #include "dwenguinoBoard.h"
 #include "dwenguinoLCD.h"
 
-volatile unsigned char spin_switch = 0;
-volatile unsigned int spin_index = 0;
-
 //hello world from wouter (^3^)
 
-unsigned int spin_mode = 0;
-
-const char left_slot[8] = {'a','e','b','f','d','c','h','g'};
-const char middle_slot[8] = {'f','h','g','b','a','c','e','d'};
-const char right_slot[8] = {'b','c','h','f','a','g','d','e'};
-
-unsigned int spent_money = 0;
-unsigned int earned_money = 0;
-
-ISR(INT4_vect){
-  spin_switch = 1;
-}
-
-ISR(TIMER1_COMPA_vect) {
-  if (spin_mode)
-  {
-    spin_index++;
-  }
+ISR(TIMER1_OVF_vect) { //triggered when the PWM timer reaches the BOTTOM value
+  PORTC ^= _BV(PC0);
 }
 
 int main(void)
 {
 
   DDRE = 0x00; //Make all PEn pins input pins
+  DDRC = _BV(PC0) | _BV(PC1); //Make pins PC0 and PC1 output pins
   PORTE |= _BV(PE4) | _BV(PE5) | _BV(PE6) | _BV(PE7); //make all the button pins activate their pullup-resistors
 
   SREG |= _BV(SREG_I); //Enable global interrupts
@@ -42,7 +24,9 @@ int main(void)
   TCCR1B |= _BV(WGM13);
   TCCR1B &= ~_BV(WGM12);
 
-  ICR1 = 0xA0; //160 : the value of TOP so we get 0.05 ms of resolution
+  ICR1 = 0x1900; //6400 : the value of TOP so one PWM 'cycle' is 8ms long
+  OCR1A = 0x640;
+  
   
   initBoard();
   initLCD();
@@ -54,6 +38,7 @@ int main(void)
     if (!(PINE & _BV(PE4)))
     {
       //Rotate servo 1 left
+      OCR1A = 0x640;
     }
     if (!(PINE & _BV(PE5)))
     {
@@ -62,11 +47,12 @@ int main(void)
     if (!(PINE & _BV(PE6)))
     {
       //Rotate servo 1 right
+      OCR1A = 0x320;
     }    
     if (!(PINE & _BV(PE7)))
     {
       //Rotate servo 2 right
-    }        
+    }
   }
   return 0;
 }
