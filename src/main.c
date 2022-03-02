33fc8758daac2bdad22b45481d31d842af8daa52
Olocool17 <ovanwassenhove@gmail.com>
2022-03-02 18:17:42 +0100

    Implement clock selection

--- src/main.c
+++ src/main.c
@@ -19,10 +19,15 @@ int main(void)
 
   SREG |= _BV(SREG_I); //Enable global interrupts
 
+  TIMSK1 |= _BV(OCIE1A);
   TCCR1A &= ~_BV(WGM11); //Enable PWM (phase / frequency correct) clock interrupts
   TCCR1A &= ~_BV(WGM10);
   TCCR1B |= _BV(WGM13);
   TCCR1B &= ~_BV(WGM12);
+
+  TCCR1B &= ~_BV(CS12); //Enable prescaling factor of 1/1 (Required for clock selection)
+  TCCR1B &= ~_BV(CS11);
+  TCCR1B |= _BV(CS10);
 
   ICR1 = 0x1900; //6400 : the value of TOP so one PWM 'cycle' is 8ms long
   OCR1A = 0x640;
