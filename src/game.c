3abfbba43d8ddae25eeac7bd997187074f6c9cdf
CreepyGar <arthur.verstraeten@gmail.com>
2022-03-30 15:26:37 +0200

    Added wordle logic + attempt function

--- src/game.c
+++ src/game.c
@@ -52,3 +52,46 @@ int rand_range(int limit)
 {
     return rand() / RAND_MAX * limit;
 }
+
+void poging(int pogingnummer, char* attempt, char* secret_word) {
+    int positie = 0;
+    char* attempt_copy = malloc(sizeof(char)*6);
+    strcpy(attempt_copy, attempt);
+    char* secret_word_copy = malloc(sizeof(char)*6);
+    strcpy(secret_word_copy, secret_word);
+    while (positie < 5) {
+        if (attempt_copy[positie] == secret_word_copy[positie]) {
+            draw_green(positie, pogingnummer - 1);
+            attempt_copy[positie] = 'O';
+            secret_word_copy[positie] = '0';
+        }
+    positie += 1;
+    }
+    int i = 0;
+    while (i < 5) {
+        if (strstr(secret_word_copy, attempt_copy[i])!=NULL && attempt_copy[i]!='O') {
+            draw_yellow(i, pogingnummer - 1);
+            int p = 0;
+            while (1) {
+                if (secret_word_copy[p] == attempt_copy[i]) {
+                    secret_word_copy[p] = '0';
+                    p = 0;
+                    attempt_copy[i] = '0';
+                    break;
+                }
+                p += 1;
+            }
+        }
+        i += 1;
+    }
+    int l = 0;
+    while (l<5) {
+        if (attempt_copy[l] != '0') {
+            draw_black(l, pogingnummer - 1);
+            attempt_copy[l] = '0';
+        l += 1;
+        }
+    }
+    free(secret_word_copy);
+    free(attempt_copy);
+}
