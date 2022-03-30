3273dd0cde9bbab8ca6b90066e7ec647690b01ec
CreepyGar <arthur.verstraeten@gmail.com>
2022-03-30 17:51:08 +0200

    Added switch cases to draw letters in poging

--- src/game.c
+++ src/game.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <time.h>
 #include <wordlelist.h>
+#include <functions.h>
 
 #include <game.h>
 #include <ui.h>
@@ -53,15 +54,70 @@ int rand_range(int limit)
     return rand() / RAND_MAX * limit;
 }
 
+//Poging: handles the visualization of the result of the attempts made by the player.
 void poging(int pogingnummer, char* attempt, char* secret_word) {
     //Create copies of attempt and secret word to preserve original ones.
     char* attempt_copy = malloc(sizeof(char)*6);
     strcpy(attempt_copy, attempt);
     char* secret_word_copy = malloc(sizeof(char)*6);
     strcpy(secret_word_copy, secret_word);
-    //First iteration: directly check for matching letters and draw green tiles if the characters match.
+    //First iteration: draws the letters on the grid and directly checks for matching letters and draws "green tiles" if the characters match.
     int positie = 0;
     while (positie < 5) {
+        switch(attempt_copy[positie]) {
+            case 'A':
+                draw_A(positie, pogingnummer - 1);
+            case 'B':
+                draw_B(positie, pogingnummer - 1);
+            case 'C':
+                draw_C(positie, pogingnummer - 1);
+            case 'D':
+                draw_D(positie, pogingnummer - 1);
+            case 'E':
+                draw_E(positie, pogingnummer - 1);
+            case 'F':
+                draw_F(positie, pogingnummer - 1);
+            case 'G':
+                draw_G(positie, pogingnummer - 1);
+            case 'H':
+                draw_H(positie, pogingnummer - 1);
+            case 'I':
+                draw_I(positie, pogingnummer - 1);
+            case 'J':
+                draw_J(positie, pogingnummer - 1);
+            case 'K':
+                draw_K(positie, pogingnummer - 1);
+            case 'L':
+                draw_L(positie, pogingnummer - 1);
+            case 'M':
+                draw_M(positie, pogingnummer - 1);
+            case 'N':
+                draw_N(positie, pogingnummer - 1);
+            case 'O':
+                draw_O(positie, pogingnummer - 1);
+            case 'P':
+                draw_P(positie, pogingnummer - 1);
+            case 'Q':
+                draw_Q(positie, pogingnummer - 1);
+            case 'R':
+                draw_R(positie, pogingnummer - 1);
+            case 'S':
+                draw_S(positie, pogingnummer - 1);
+            case 'T':
+                draw_T(positie, pogingnummer - 1);
+            case 'U':
+                draw_U(positie, pogingnummer - 1);
+            case 'V':
+                draw_V(positie, pogingnummer - 1);
+            case 'W':
+                draw_W(positie, pogingnummer - 1);
+            case 'X':
+                draw_X(positie, pogingnummer - 1);
+            case 'Y':
+                draw_Y(positie, pogingnummer - 1);
+            case 'Z':
+                draw_Z(positie, pogingnummer - 1);
+        }
         if (attempt_copy[positie] == secret_word_copy[positie]) {
             draw_green(positie, pogingnummer - 1);
             attempt_copy[positie] = '0';
@@ -70,7 +126,7 @@ void poging(int pogingnummer, char* attempt, char* secret_word) {
     positie += 1;
     }
     //Second iteration: check for the remaining letters in the attempt if they appear anywhere in the remainder of the secret word.
-    //If a match is found it will immediately set the first instances of the letter in both strings to 0 and draw a yellow tile on the place of the first
+    //If a match is found it will immediately set the first instances of the letter in both strings to 0 and draw a "yellow tile" on the place of the first
     //instance of this letter in the attempt. This correctly works out for duplicates from both sides :).
     int i = 0;
     while (i < 5) {
@@ -89,7 +145,7 @@ void poging(int pogingnummer, char* attempt, char* secret_word) {
         }
         i += 1;
     }
-    //Third iteration: drawing black tiles for every tile that has not receieved a color in the previous 2 iterations.
+    //Third iteration: drawing "black tiles" for every tile that has not receieved a color in the previous 2 iterations.
     int l = 0;
     while (l<5) {
         if (attempt_copy[l] != '0') {
