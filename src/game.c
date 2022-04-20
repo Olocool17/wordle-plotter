e99ab9cdb2def2fa9b91b2015a71b3b2c350dc92
CreepyGar <arthur.verstraeten@gmail.com>
2022-04-20 15:46:48 +0200

    more cleanup

--- src/game.c
+++ src/game.c
@@ -11,6 +11,7 @@
 
 void wordle(bool random) 
 {
+    //word selection phase
     char* wordle_word = malloc(sizeof(char)*6);
     if (random) 
     {
@@ -25,9 +26,9 @@ void wordle(bool random)
         if ((strcmp(wordle_word, "00000") == 0))
         {
             display_menu(game_info_menu("exiting game...", ""), NULL);
-            return;
         }
     }
+    //word guessing phase
     bool game_won = false;
     for(size_t attempt_count = 0; (attempt_count < 5) && (!game_won); attempt_count++)
     {
@@ -36,7 +37,6 @@ void wordle(bool random)
         {
             display_menu(game_info_menu("exiting game...", ""), NULL);
             free(wordle_word);
-            return;
         }
         attempt(attempt_count, attempt_word, wordle_word);
         if(attempt_word == wordle_word)
@@ -45,6 +45,7 @@ void wordle(bool random)
         }
         free(attempt_word);
     }
+    //final game state check
     if (game_won)
     {
         display_menu(game_info_menu("victory", wordle_word), NULL);
@@ -59,7 +60,7 @@ void wordle(bool random)
 char* manual_word_select() 
 {
     char* word = word_select();
-    if((strcmp(word, "00000") == 0)  && (!word_in_list(word, (char**) wordle_list))  ) 
+    if((strcmp(word, "00000") == 0) && (!word_in_list(word, (char**) wordle_list))) 
     {
         game_error_menu("word not found");
         return manual_word_select();
@@ -70,12 +71,9 @@ char* manual_word_select()
 bool word_in_list(char* word, char** list) 
 {
     int wordlist_count = sizeof(wordle_list) / sizeof(wordle_list[0]);
-    for (size_t i = 0; i < wordlist_count; i++) 
+    for (size_t i = 0; (i < wordlist_count) && (strcmp(word, list[i]) == 0); i++) 
     {
-        if (strcmp(word, list[i]) == 0) 
-        {
-            return true;
-        }
+        return true;
     }
     return false;
 }
@@ -88,26 +86,26 @@ int rand_range(int limit)
 
 void attempt(int attempt_number, char* attempt, char* secret_word) 
 {
-//Create copies of attempt and secret word to preserve original ones.
+//create copies of attempt and secret word
     char* attempt_copy = malloc(sizeof(char)*6);
     strcpy(attempt_copy, attempt);
     char* secret_word_copy = malloc(sizeof(char)*6);
     strcpy(secret_word_copy, secret_word);    
-//First iteration: draws the letters on the grid and directly checks for matching letters and draws "green tiles" if the characters match.
-    for (size_t position = 0; position < 5; position++) 
+//first iteration: draws the letters on the grid and directly checks for matching letters and draws "green tiles" if the characters match.
+    for (size_t i = 0; i < 5; i++) 
     {
-        draw_letter_on_grid(attempt_copy[position], position, attempt_number);
-        if (attempt_copy[position] == secret_word_copy[position]) 
+        draw_letter_on_grid(attempt_copy[i], i, attempt_number);
+        if (attempt_copy[i] == secret_word_copy[i]) 
         {
-            draw_letter_on_grid('g', position, attempt_number);
-            attempt_copy[position] = '0';
-            secret_word_copy[position] = '0';
+            draw_letter_on_grid('g', i, attempt_number);
+            attempt_copy[i] = '0';
+            secret_word_copy[i] = '0';
         }
     }
 /* 
-    second iteration: check for the remaining letters in the attempt if they appear anywhere in the remainder of the secret word
-    if a match is found it will immediately set the first instances of the letter in both strings to 0 and draw a "yellow tile" on the place of the first
-    instance of this letter in the attempt
+    second iteration: check for the remaining letters in if they appear anywhere in the remainder of the secret word
+    if a match is found it will set the first instances of the letter in both strings to 0 and draw a "yellow tile" on the place of the first instance of
+    this letter in the attempt
 */  
     for (size_t i = 0; (i < 5) && ((strchr(secret_word_copy, attempt_copy[i]) != NULL) && (attempt_copy[i] != '0')); i++) 
     { 
@@ -120,9 +118,10 @@ void attempt(int attempt_number, char* attempt, char* secret_word)
             break; 
         }
     }
-//Third iteration: drawing "black tiles" for every letter that hasn't been assigned a color yet.
-    for (size_t l=0; (l < 5) && (attempt_copy[l] != '0'); l++) {
-        draw_letter_on_grid('b', l, attempt_number);
+//third iteration: drawing "black tiles" for every letter that hasn't been assigned a color yet.
+    for (size_t i=0; (i < 5) && (attempt_copy[i] != '0'); i++) 
+    {
+        draw_letter_on_grid('b', i, attempt_number);
     }
     free(secret_word_copy);
     free(attempt_copy);
